30f83c73eb6894e17a56389f90a1f32142156679
Pierre enoc <pierre.enoc@gympass.com>
2021-07-17 19:19:26 -0300

    :sparkles: add event class

--- desafio-eventstore/src/main/java/br/com/pierre/desafioeventstore/Event.java
+++ desafio-eventstore/src/main/java/br/com/pierre/desafioeventstore/Event.java
@@ -1,4 +1,33 @@
 package br.com.pierre.desafioeventstore;
 
+import java.time.LocalDateTime;
+
 public class Event {
+    private String type;
+    private LocalDateTime moment;
+
+    public Event() {
+
+    }
+
+    public Event(String type, LocalDateTime moment) {
+        this.type = type;
+        this.moment = moment;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getMoment() {
+        return moment;
+    }
+
+    public void setMoment(LocalDateTime moment) {
+        this.moment = moment;
+    }
 }
